4b28a53928647c8dd5c91d19caf0a2bcdac6e701
zfw <zfw15@mails.tsinghua.edu.cn>
2018-04-25 23:22:14 +0800

    new data

--- patchFrame.py
+++ patchFrame.py
@@ -11,6 +11,8 @@ with open('location_aq.txt', 'r') as locFile:
         locList_aq.append(lis[0])
         line = locFile.readline()
 
+print(locList_aq)
+
 locList_me = []
 with open('location_me.txt', 'r') as locFile:
     line = locFile.readline()
@@ -32,23 +34,23 @@ with open('London_AirQuality_Stations.csv', 'r') as locFile:
 for name in locList_aq:
     filename = './processedData/'+name+'.csv'
     df = pd.read_csv(filename,header=0,index_col=0)
-    df.fillna(method='pad')
-    df.fillna(method='bfill')
-    df.fillna(-1)
+    df.fillna(method='pad',inplace=True)
+    df.fillna(method='bfill',inplace=True)
+    df.fillna(-1,inplace=True)
     df.to_csv(filename)
 
 for name in locList_me:
     filename = './processedData/'+name+'.csv'
     df = pd.read_csv(filename,header=0,index_col=0)
-    df.fillna(method='pad')
-    df.fillna(method='bfill')
-    df.fillna(-1)
+    df.fillna(method='pad',inplace=True)
+    df.fillna(method='bfill',inplace=True)
+    df.fillna(-1,inplace=True)
     df.to_csv(filename)
 
 for name in locList_aq_ld:
     filename = './processedData/london/'+name+'.csv'
     df = pd.read_csv(filename,header=0,index_col=0)
-    df.fillna(method='pad')
-    df.fillna(method='bfill')
-    df.fillna(-1)
+    df.fillna(method='pad',inplace=True)
+    df.fillna(method='bfill',inplace=True)
+    df.fillna(-1,inplace=True)
     df.to_csv(filename)
